16bfddf4709e7dd74389a6e0af683627622686fc
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-01-29 10:20:10 +0530

    more dependencies added and base code added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { url } = require('inspector');
 const app = express();
 
 const PORT = 1337;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded( {extended:false} ));
 
 app.listen(PORT, () => {
   console.log("App running on PORT : " , PORT);
