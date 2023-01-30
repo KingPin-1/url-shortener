5ba7e3777e98ce255b0d98c40bba33e7ddbe18a3
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-01-31 01:58:10 +0530

    base code done

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const { url } = require('inspector');
+const express = require("express");
+const { url } = require("inspector");
 const app = express();
 
 const PORT = 1337;
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded( {extended:false} ));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
-  console.log("App running on PORT : " , PORT);
-})
+    console.log(`App is running at http://localhost:${PORT} `);
+});
