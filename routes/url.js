4002ea9fe68bc2d26fb151028fe4778d2e33b26f
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-01-31 05:02:13 +0530

    Completed application --> couldn't fix nanoid so used math random to generate short urls

--- routes/url.js
+++ routes/url.js
@@ -1,34 +1,51 @@
 const express = require("express");
 const router = express.Router();
 // import { nanoid } from 'nanoid';
-const createDB = require('../config/db');
-const URL = require('../models/urlModels');
+const createDB = require("../config/db");
+const URL = require("../models/urlModels");
 
 const baseURL = "http://localhost:1337/urlapi";
 
 createDB.sync().then(() => {
     console.log("DB IS RUNNING!");
-})
+});
 
-router.post('/', async (req,res) => {
-    try{
-        const { longURL } = req.body; 
+router.post("/", async (req, res) => {
+    try {
+        const { longURL } = req.body;
         // any calls by user to hostname + id will redirect to longURL
         // longURL to id
-        const shortID = Math.random();
-        // store in db 
+        const shortID = Math.floor(Math.random()*10000);
+        // store in db
         const shortURL = await URL.create({
-            longURL,shortURL:shortID
+            longURL,
+            shortURL: shortID,
         });
 
         return res.status(200).json({
             status: "ok",
-            shortURL: `${baseURL}/${shortID}`
+            shortURL: `${baseURL}/${shortID}`,
         });
-
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
-})
+});
+
+router.get("/:short", async (req, res) => {
+    let shortID = req.params.short;
+    try {
+        const url = await URL.findOne({
+            where: { 
+                shortURL: shortID 
+            }
+        });
+        if (!url) {
+            return res.status(404).send("Invalid Short url");
+        }
+        return res.redirect(url.longURL);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
 
 module.exports = router;
